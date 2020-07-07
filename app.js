a08c43592905c7cb6f5948f02841ee9b7a6fe984
oscarzamora <oscarnewsletter1@gmail.com>
2020-07-07 16:46:44 -0500

    agregar mongoose.conection a una constante para no escribirlo repetidas veces

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const uri = "mongodb://127.0.0.1:27017/mywebstore";
+const db = mongoose.connection;
 
 mongoose
   .connect(uri, {
@@ -10,10 +11,10 @@ mongoose
   .catch((err) => console.log(err));
 
 //Decirme cuando la base de datos ha sido conectada
-mongoose.connection.on("open", (_) => {
+db.on("open", (_) => {
   console.log("Base de datos conectada a", uri);
 });
 //Decirme cuando hay un error a la conexion de bd
-mongoose.connection.on("error", (err) => {
+db.on("error", (err) => {
   console.log(err);
 });
