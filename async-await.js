fce0a9f1ee89050fbe1b56aa4c213b4597e62add
oscarzamora <oscarnewsletter1@gmail.com>
2020-07-07 23:14:44 -0500

    crear un dato con async-await

---
+++ async-await.js
@@ -0,0 +1,16 @@
+require("./connection");
+
+const Product = require("./models/Product");
+
+async function main() {
+  const product = new Product({
+    name: "keyboard",
+    description: "corsair a68 rgb gaming keyboard",
+    price: 150,
+  });
+
+  const productSave = await product.save();
+  console.log(productSave);
+}
+
+main();
