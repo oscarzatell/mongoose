f56ac951d0c42692a6898ff0dfbecf37e9881132
oscarzamora <oscarnewsletter1@gmail.com>
2020-07-08 18:54:48 -0500

    Buscar en todos los datos y borrar solo uno

--- crud/delete.js
+++ crud/delete.js
@@ -3,7 +3,8 @@ require("../connection");
 const Product = require("../models/Product");
 
 const someFuction = async () => {
-  const result = await Product.deleteMany({ name: "keyboard" });
+  //const result = await Product.deleteMany({ name: "keyboard" });
+  const result = await Product.deleteOne({ name: "laptop hp" }); //esto solo eliminará un dato
   console.log(result);
 };
 
