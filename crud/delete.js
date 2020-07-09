767a58deff12a7f9f51c2c7ba19ba99a5991ceed
oscarzamora <oscarnewsletter1@gmail.com>
2020-07-08 19:10:27 -0500

    borrar un dato por ID

--- crud/delete.js
+++ crud/delete.js
@@ -5,7 +5,8 @@ const Product = require("../models/Product");
 const someFuction = async () => {
   //const result = await Product.deleteMany({ name: "keyboard" });
   //const result = await Product.deleteOne({ name: "laptop hp" }); //esto solo eliminará un dato
-  const result = await Product.findOneAndDelete({ name: "laptop" });
+  //const result = await Product.findOneAndDelete({ name: "laptop" });
+  const result = await Product.findByIdAndDelete("5f06275d9f8deb393ce798dc");
   console.log(result);
 };
 
