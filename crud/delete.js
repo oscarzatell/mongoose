e43b785a3955422d7beba44430a113729b36ef6c
oscarzamora <oscarnewsletter1@gmail.com>
2020-07-08 19:01:54 -0500

    Buscar 1 dato y borrarlo

--- crud/delete.js
+++ crud/delete.js
@@ -4,7 +4,8 @@ const Product = require("../models/Product");
 
 const someFuction = async () => {
   //const result = await Product.deleteMany({ name: "keyboard" });
-  const result = await Product.deleteOne({ name: "laptop hp" }); //esto solo eliminará un dato
+  //const result = await Product.deleteOne({ name: "laptop hp" }); //esto solo eliminará un dato
+  const result = await Product.findOneAndDelete({ name: "laptop" });
   console.log(result);
 };
 
